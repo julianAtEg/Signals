f54342f16bec04d226d582f7e655e3e9bb7d125f
Julian <julian.gold@talk21.com>
2016-06-26 14:45:28 +0000

    o Fixed problem with Maw audio o Can now enable / disable ring menu items

--- Source/Signals/ActionMenuItem.h
+++ Source/Signals/ActionMenuItem.h
@@ -8,21 +8,29 @@ struct FActionMenuItem
 {
 	GENERATED_USTRUCT_BODY()
 
+	// Item (terse) text.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
 	FString Text;
 
+	// Index of the icon to use in the menu (see SignalsBattleModeBP)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
 	int IconIndex;
 
+	// Verbose description of the item.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
 	FString Description;
 
+	// Unique ID of the item.
 	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "UI")
 	int ID;
 
 	// Is this an action node or a category node?
 	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "UI")
 	bool IsLeaf;
+
+	// Can the user choose this option?
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "UI")
+	bool IsSelectable;
 
 	// Menu level. 0 is action bar, 1 is ring menu.
 	int Level;
